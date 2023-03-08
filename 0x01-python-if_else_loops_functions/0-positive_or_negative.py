3e803579aa352b2fd16842f029049a468c1a404b
IbroDante <ibroman02@yahoo.com>
2023-03-08 08:07:41 +0000

    commit on randon numbers

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-i = 1
-while(i = number):
-    i += 1
-    print("is positive: ", number)
+if number > 0:
+    print("{:d} is positive".format(number))
+elif number == 0:
+    print("{:d} is zero".format(number))
+else:
+    print("{:d} is negative".format(number))
