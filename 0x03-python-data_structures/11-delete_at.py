78c3107e3e1e5057c9cce05ae7c376587885b227
IbroDante <ibroman02@yahoo.com>
2023-03-12 16:33:35 +0000

    this another commit on data structure1

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
+    length = len(my_list)
 
-    for i in range(idx, len(my_list) - 1):
-        my_list[i] = my_list[i + 1]
+    if idx < 0 or idx >= length:
+        return (my_list)
 
-    my_list = my_list[:-1]
+    del my_list[idx]
 
-    return my_list
+    return (my_list)
