e2a42070973d83b5786314128252e81ec7662b2e
IbroDante <ibroman02@yahoo.com>
2023-03-12 13:45:08 +0000

    this another commit on data structure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string =""
-    for i in range(len(my_string)):
-        if my_string[i] != 'c' and my_string[i] != 'C':
-            new_string += my_string[i]
-    return new_string
+    result = ""
+    for char in my_string:
+        if char not in ['c', 'C']:
+            result += char
+    return result
