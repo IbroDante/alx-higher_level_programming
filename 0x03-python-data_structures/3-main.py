05341d99eaffd70501cee554c568ee6343b2ed66
IbroDante <ibroman02@yahoo.com>
2023-03-12 14:32:17 +0000

    this another commit on data structure1

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-import_name = __import__('3-print_reversed_list_integer')
-print_reversed_list_integer = import_name.print_reversed_list_integer
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
 my_list = [1, 2, 3, 4, 5]
 print_reversed_list_integer(my_list)
