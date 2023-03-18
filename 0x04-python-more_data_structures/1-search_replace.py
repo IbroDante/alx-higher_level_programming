c90d9ce561ccd9cf2f06f61392bca36338da2e6d
IbroDante <ibroman02@yahoo.com>
2023-03-18 22:08:06 +0000

    commit on data structure

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-  new_list = []
-  for i in my_list:
-    if i == search:
-      new_list.append(replace)
-    else:
-      new_list.append(i)
-  return new_list
+    new_list = []
+    for element in my_list:
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
+    return new_list
