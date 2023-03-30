f9708cdd6913c8da424ce18147c791cdde8de2eb
IbroDante <ibroman02@yahoo.com>
2023-03-30 17:03:27 +0000

    update on exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
-  try:
-    return fct(*args)
-  except Exception as error:
-    print('Exception:', error)
-    return None
+    try:
+        return fct(*args)
+    except Exception as error:
+        print('Exception:', error)
+        return None
