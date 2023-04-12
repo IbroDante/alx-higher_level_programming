937a15432b252a49f453cab4805df188922cb72e
IbroDante <ibroman02@yahoo.com>
2023-04-12 15:09:36 +0000

    updated

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
     with open(filename, 'w', encoding='utf-8') as f:
-        num = f.write(text)
-    return num
+        return f.write(text)
