9bca83087b911a9a344306c707bad1b0fc7f2402
IbroDante <ibroman02@yahoo.com>
2023-04-30 16:06:39 +0000

    updates

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,54 +1,57 @@
 #!/usr/bin/python3
-""" Module to print status code """
-import sys
+"""Reads from standard input and computes metrics.
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
+"""
 
 
-class Magic:
-    def __init__(self):
-        self.dic = {}
-        self.size = 0
-
-    def init_dic(self):
-        self.dic = {'200': 0,
-                    '301': 0,
-                    '400': 0,
-                    '401': 0,
-                    '403': 0,
-                    '404': 0,
-                    '405': 0,
-                    '500': 0}
-
-    def add_status_code(self, status):
-        if status in self.dic:
-            self.dic[status] += 1
-
-    def print_info(self):
-        print("File size: {}".format(self.size))
-        for key in sorted(self.dic.keys()):
-            if self.dic[key] != 0:
-                print("{}: {}".format(key, self.dic[key]))
-
-    def signal_handler(self, sig, frame):
-        self.print_info()
-        sys.exit(0)
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
 
 if __name__ == "__main__":
-    magic = Magic()
-    magic.init_dic()
-    nlines = 0
+    import sys
 
-    for line in sys.stdin:
-        try:
-            list_line = [x for x in line.split(" ") if x.strip()]
-            magic.add_status_code(list_line[-2])
-            magic.size += int(list_line[-1].strip("\n"))
-        except:
-            pass
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
-        nlines += 1
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
 
-        if nlines % 10 == 0:
-            magic.print_info()
+            line = line.split()
 
-    magic.print_info()
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
