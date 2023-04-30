b87b8e40db1f5a69690cc89ac83308e103369093
IbroDante <ibroman02@yahoo.com>
2023-04-30 18:43:10 +0000

    commit on inherrintace

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define a base geo class"""
+
+
+class BaseGeometry:
+    """ Empty class """
+
+    def area(self):
+        """not implemented"""
+        raise Exception("area() is not implemented")
